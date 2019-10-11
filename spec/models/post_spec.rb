693107c90de2b6b0bfada91461a29f18ff7a14ed
Ana Silva <ananunesdasilva@gmail.com>
2019-10-11 18:45:27 +0100

    Change create to build (not necessary to actually persist)

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe '#photo' do
-    let(:post) { create(:post) }
+    let(:post) { build(:post) }
 
-    it {nexpect(post.photo).to be_an_instance_of(ActiveStorage::Attached::One) }
+    it { expect(post.photo).to be_an_instance_of(ActiveStorage::Attached::One) }
   end
 end
