edc1fadc36d2f5d77a6eb950db569b5a2649455d
Ana Silva <ananunesdasilva@gmail.com>
2019-10-14 23:27:09 +0100

    Creating thumbnails

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,9 +1,9 @@
 class PostSerializer < ActiveModel::Serializer  
   include Rails.application.routes.url_helpers
 
-  attributes :id, :created_at, :updated_at, :photo
+  attributes :id, :created_at, :updated_at, :photo_url
 
-  def photo
-    rails_blob_path(object.photo, only_path: true) if object.photo.attached?
+  def photo_url
+    rails_representation_path(object.photo.variant(resize_to_limit: [100, 100]), only_path: true) if object.photo.attached?
   end
 end
