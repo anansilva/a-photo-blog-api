a45c8b6f443f593199cb505fe5e621045567c4d9
Ana Silva <ananunesdasilva@gmail.com>
2019-10-18 22:52:03 +0100

    Return token whe logged in

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,10 +1,35 @@
-class AuthenticationsController < ApplicationController
+class AuthenticationsController < ApplicationController  
   def login
-    user = User.find_by_email(params[:email])
-    if user&.authenticate(params[:password])
-      render status: :ok
+    if find_user&.authenticate(login_params[:password])
+      token = Services::Authentications::JsonWebToken.encode(
+        { user_id: find_user.id, user_email: find_user.email }, 
+        expiring_date
+      )
+      
+      render json: { token: token, 
+                     exp: expiring_date,
+                     user_email: find_user.email
+                    }, status: :ok
     else
       render status: :unauthorized
     end        
+  end
+
+  private
+
+  def login_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def payload(user_id, user_email)
+    { user_id: user_id, user_email: user_email }
+  end
+
+  def expiring_date
+    @expiring_date ||= 24.hours.from_now
+  end
+
+  def find_user
+    @find_user ||= User.find_by_email(login_params[:email])
   end
 end
--- spec/controllers/authentications_controller_spec.rb
+++ spec/controllers/authentications_controller_spec.rb
@@ -7,24 +7,28 @@ RSpec.describe AuthenticationsController, type: :controller do
     end
 
     context 'when providing a valid email/password combination' do
+      let(:login_params) { { email: 'user@example.com', password: 'password' } }
+
       it 'responds successfully' do
-        post :login, params: { email: 'user@example.com', password: 'password' }
+        post :login, params: { user: login_params }
 
         is_expected.to respond_with :ok
       end
 
-      it 'returns the user' do
-        post :login, params: { email: 'user@example.com', password: 'password' }
+      it 'returns the token, expiring date and user email' do
+        post :login, params: { user: login_params }
         
         parsed_response = JSON.parse(response.body)
 
-        expect(JSON.parse(response.body)['id']).to eq(User.first.id)
+        expect(JSON.parse(response.body).keys).to include('token', 'exp', 'user_email')
       end
     end
 
     context 'when providing an invalid email/password combination' do
+      let(:login_params) { { email: 'user@example.com', password: 'notmypassword' } }
+      
       it 'responds successfully' do
-        post :login, params: { email: 'user@example.com', password: 'notmypassword' }
+        post :login, params: {user: login_params } 
         
         is_expected.to respond_with :unauthorized
       end
