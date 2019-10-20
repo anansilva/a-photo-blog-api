98e75d85b9a2b00e0ac8ceeb82beca3bfeecbc55
Ana Silva <ananunesdasilva@gmail.com>
2019-10-20 17:16:25 +0100

    Change secrets to credentials

--- app/lib/services/authentications/json_web_token.rb
+++ app/lib/services/authentications/json_web_token.rb
@@ -1,8 +1,8 @@
 module Services
   module Authentications
     class JsonWebToken
-      SECRET_KEY = Rails.application.secrets.secret_key_base
-      
+      SECRET_KEY = Rails.application.credentials.secret_key_base
+
       def self.encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
         JWT.encode(payload, SECRET_KEY)
