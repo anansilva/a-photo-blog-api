1eb5dfba3641e0dc0a721cab8134fc2d16873dc3
Ana Silva <ananunesdasilva@gmail.com>
2019-10-20 23:23:15 +0100

    Reduce variant size

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -4,6 +4,6 @@ class PostSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :updated_at, :photo_thumbnail_url
 
   def photo_thumbnail_url
-    rails_representation_path(object.photo.variant(resize_to_limit: [200, 200]), only_path: true) if object.photo.attached?
+    rails_representation_path(object.photo.variant(resize_to_limit: [100, 100]), only_path: true) if object.photo.attached?
   end
 end
